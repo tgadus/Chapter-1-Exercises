243b0a57f9d2b47324ec04da70ef8f2b463a5bfe
tgadus <tgadus@iu.edu>
2025-09-02 22:59:45 -0400

    Add files via upload

---
+++ Exercise01_02.java
@@ -0,0 +1,9 @@
+public class Exercise01_01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+	System.out.printIn("Welcome to Java");
+	System.out.printIn("welcome to Java"):
+    }
+}
